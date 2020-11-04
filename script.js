b9370eb35442940ff016ed6b8d287f141abfcb44
Drew Worden <drewpworden@pop-os.localdomain>
2020-11-03 21:13:15 -0500

    Added homepage animation

---
+++ script.js
@@ -0,0 +1,15 @@
+let introContainer = anime({
+    targets: ".intro-container",
+    opacity: 1,
+    duration: 1500,
+    easing: "easeInOutSine"
+});
+
+let homeButton = anime({
+    targets: ".home-button",
+    delay: 1500,
+    duration: 500,
+    scale: 1.25,
+    direction: "alternate",
+    easing: "easeInOutSine"
+})
